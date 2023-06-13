6e6aa474308935d90fa91fa29dc493924a6fb531
rasi235 <136437821+rasi235@users.noreply.github.com>
2023-06-13 14:49:49 +0530

    Update prediction_web.py

--- prediction_web.py
+++ prediction_web.py
@@ -5,7 +5,7 @@ import pickle
 
 
 # loading the saved model
-loaded_model=pickle.load(open('C:/Users\pavani.pasupulati\OneDrive - Anblicks\Desktop\MACHINE LEARING/trained_model.model','rb')) 
+loaded_model=pickle.load(open('trained_model.model','rb')) 
 
 
 def  heart_prediction(input_data):
@@ -29,21 +29,21 @@ def  heart_prediction(input_data):
 def model():
 
 
-    st.title("heart prediction web app")
+    st.title("Heart prediction web app")
     #  age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal
-    age=st.text_input("age of the person")
-    sex=st.text_input("male or female")
-    cp=st.text_input("how many Constrictive pericarditis ")
-    trestbps=st.text_input("how much blood sugar")
-    chol=st.text_input("cholesterol")
-    fbs=st.text_input("Fasting glucose levels")
-    restecg=st.text_input(" Resting electrocardiographic measurement ")
-    thalach=st.text_input("The person's maximum heart rate achieved")
-    exang=st.text_input("exercise induced angina")
-    oldpeak=st.text_input("ST depression induced by exercise relative to res")
-    slope=st.text_input("The ST segment shift relative to exercise-induced increments in heart rate")
-    ca= st.text_input("The number of major vessels ")
-    thal=st.text_input("The persons maximum heart rate achieved")
+    age=st.text_input("Age of the person")
+    sex=st.text_input("Male or Female")
+    cp=st.text_input("CP(How Many Constrictive Pericarditis)")
+    trestbps=st.text_input("TRESTPS(How Much Blood Sugar)")
+    chol=st.text_input("CHOL(Cholesterol)")
+    fbs=st.text_input("FBS(Fasting Glucose Levels)")
+    restecg=st.text_input("RESTECG(Resting Electrocardiographic Measurement)")
+    thalach=st.text_input("THALACH(The Person's Maximum Heart Rate Achieved)")
+    exang=st.text_input("EXANG(Exercise Induced Angina)")
+    oldpeak=st.text_input("OLDPEAK(ST Depression Induced By Exercise Relative To Res)")
+    slope=st.text_input("SLOPE(The ST Segment Shift Relative To Exercise-Induced Increments In Heart Rate)")
+    ca= st.text_input("CA(The Number of Major Vessels)")
+    thal=st.text_input("THAL(The persons maximum heart rate achieved)")
 
 
     ccg =''
