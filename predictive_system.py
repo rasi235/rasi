0a00f4574fa3913f68c718cc43dbaefa793f78cf
rasi235 <136437821+rasi235@users.noreply.github.com>
2023-06-13 14:51:26 +0530

    Update predictive_system.py

--- predictive_system.py
+++ predictive_system.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 
 # loading the saved model
-loaded_model=pickle.load(open('C:/Users\pavani.pasupulati\OneDrive - Anblicks\Desktop\MACHINE LEARING/trained_model.model','rb')) 
+loaded_model=pickle.load(open('trained_model.model','rb')) 
 input_data = ( 41,1,0,110,172,0,0,158,0,0,2,0,3)
 
 input_data_as_numpy_array =np.asarray(input_data)
@@ -12,8 +12,8 @@ prediction = loaded_model.predict(input_data_reshaped)
 print(prediction)
 
 if (prediction[0] ==0):
-  print(" the person  have a heart disease")
+  print("Person is Defective")
 else:
-  print("the person doesnt  has heart disease")
+  print("Person is healthy")
 
 # st.display(prediction)
